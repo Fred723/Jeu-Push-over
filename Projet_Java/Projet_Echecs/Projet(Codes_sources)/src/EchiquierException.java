e8e36da689e193c2645e1cd148315b8809cc10e8
frednad <corbeille.i@laposte.net>
2020-07-27 11:31:28 +0200

    Remove old projects

--- Projet_Java/Projet_Echecs/Projet(Codes_sources)/src/EchiquierException.java
+++
@@ -1,7 +0,0 @@
-public class EchiquierException extends Exception {
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	public EchiquierException(){ super();    }
-    public EchiquierException(String m){ super(m);    } }
